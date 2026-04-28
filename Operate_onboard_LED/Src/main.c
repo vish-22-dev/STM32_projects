393006118cfe766a7c94ba81413c01b74d6a2c57
Vishal_Pandule <vishalpandule1603@gmail.com>
2026-04-28 13:55:59 +0530

    Toggle the LED using onboard push button

--- Operate_onboard_LED/Src/main.c
+++ Operate_onboard_LED/Src/main.c
@@ -1,57 +1,52 @@
-/**
- * @file main.c
- * @brief Onboard LED Control using GPIO
- * 
- * This program demonstrates GPIO configuration on STM32F446RE microcontroller.
- * It configures GPIOA Pin 5 (PA5) as output for controlling an onboard LED, and GPIOB Pin 3 (PB3)
- * as input with pull-down. The LED is toggled based on the input pin 3 state:
- * - When PB3 goes HIGH, LED turns ON (PA5 goes HIGH)
- * - When PB3 goes LOW, LED turns OFF (PA5 goes LOW)
- * 
- * Hardware Connections:
- * - PA5: Connected to onboard LED
- * - PB3: Input pin (3.3V or GND for testing)
- * 
- * Registers Used:
- * - RCC PCLKR (0x40023830): Enable clock for GPIO ports
- * - GPIOx MODER: Configure pin mode (input/output)
- * - GPIOx PUPDR: Configure pull-up/pull-down
- * - GPIOx ODR: Output data register (write LED state)
- * - GPIOx IDR: Input data register (read pin state)
- */
+/*
+@file main.c
+@brief Onboard LED Control using GPIO
+
+This program demonstrates GPIO configuration on STM32F446RE microcontroller.
+It configures GPIOA Pin 5 (PA5) as output for controlling an LED, and GPIOC Pin 13 (PC13)
+as input with pull-up for a button. The LED is toggled based on the button state:
+- When button is pressed (PC13 goes LOW), LED turns ON (PA5 goes HIGH)
+- When button is released (PC13 goes HIGH), LED turns OFF (PA5 goes LOW)
+
+Hardware Connections:
+- PA5: Connected to onboard LED
+- PC13: Connected to user button with internal pull-up
+
+Registers Used:
+- RCC PCLKR (0x40023830): Enable clock for GPIO ports A and C
+- GPIOx MODER: Configure pin mode (input/output)
+- GPIOx PUPDR: Configure pull-up/pull-down
+- GPIOx ODR: Output data register (write LED state)
+- GPIOx IDR: Input data register (read button state)
+*/
 
 #include <stdint.h>
-void delay_ms(uint32_t ms)
-{
-    for(uint32_t i = 0; i < ms; i++)
-    {
-    	for(volatile uint32_t j = 0; j < 2000; j++);
-    }
-}
 int main(void)
 {
     uint32_t* PCLKR = (uint32_t*)0x40023830;
     uint32_t* PMODER_A = (uint32_t*)0x40020000;
-    uint32_t* PMODER_B = (uint32_t*)0x40020400;
+    uint32_t* PMODER_C = (uint32_t*)0x40020800;
     uint32_t* PODR_A = (uint32_t*)0x40020014;
-    uint32_t* PIDR_B = (uint32_t*)0x40020410;
-    uint32_t* PPUPDR_B = (uint32_t*)0x4002040C;
-
+    uint32_t* PIDR_C = (uint32_t*)0x40020810;
+    uint32_t* PPUPDR_C = (uint32_t*)0x4002080C;
 
     *PCLKR |=(1<<0);	//make port A enable
-    *PCLKR |=(1<<1);	//make port B enable
+    *PCLKR |=(1<<2);	//make port C enable
 
+    // PA5 ---> Output
     *PMODER_A &= ~(3<<(5*2)); //reset the 10th bit MODER A register
     *PMODER_A |= (1<<(5*2)); // Set the 10th bit of MODER A register
 
-    *PMODER_B &= ~(3<<(3*2)); // It set PB3 as input pin
+    // PC ----> Input
+    *PMODER_C &= ~(3<<(13*2)); // It set PC13 as input pin
 
-    *PPUPDR_B &= ~(3<<(3*2));
-    *PPUPDR_B |= (2<<(3*2));    // for PB3, Pull down is enabled
+    //Pull up
+    *PPUPDR_C &= ~(3<<(13*2));
+    *PPUPDR_C |= (1<<(13*2));    // for PC13, Pull up is enabled
 
     while(1)
     {
-    	if(*PIDR_B & (1<<3))
+    	if(!(*PIDR_C & (1<<13)))
 		{
     		*PODR_A |= (1<<5);   // LED ON
 		}
